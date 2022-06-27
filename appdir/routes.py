93fc62e33d39745351890dac5f56180620e31eb4
BruteForce009 <1517satyamtomar@gmail.com>
2022-06-27 08:27:47 +0530

    Updating routes

--- appdir/routes.py
+++ appdir/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template, url_for, flash, redirect, get_flashed_messages, request
 from appdir import app, db
 import appdir.models
+import json
 # from appdir.models import User
 import time
 
 
-@app.route('/', methods=['GET','POST'])
-@app.route("/data", methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
+@app.route("/data", methods=['GET', 'POST'])
 def data():
     NodeID = request.args.get('NodeID')
     pm1 = request.args.get('tpluviometer1')
@@ -27,6 +28,39 @@ def data():
     sensors = appdir.models.SensorData.query.all()
     return render_template('data_.html', sensors=sensors)
 
+
+@app.route("/datx?NodeID=<NodeID>&tpluviometer1=<pm1>&tpluviometer2=<pm2>&tpluviometer3=<pm3>&tanemometer=<am>&twd=<vane_str>&tSoil_moist=<sm>&ttemp=<temp>&thumd=<humd>&tpres=<pres>&tLuminosity=<lum>&tbat=<int:bat>&ttime=<timex>", methods=['GET', 'POST'])
+def datx(NodeID, pm1, pm2, pm3, am, vane_str, sm, temp, humd, pres, lum, bat, timex):
+    sensordata = appdir.models.SensorData(NodeID=NodeID, tpluviometer1=pm1, tpluviometer2=pm2, tpluviometer3=pm3, tanemometer=am, twd=vane_str, tSoil_moist=sm, ttemp=temp, thumd=humd, tpres=pres, tLuminosity=lum, tbat=bat, ttime=timex)
+    db.session.add(sensordata)
+    db.session.commit()
+    sensors = appdir.models.SensorData.query.all()
+    return render_template('data_.html', sensors=sensors)
+
+
+@app.route("/jsondata", methods=['POST'])
+def jsondata():
+    request_data = request.get_json()
+
+    NodeID = request_data['NodeID']
+    pm1 = request_data['tpluviometer1']
+    pm2 = request_data['tpluviometer2']
+    pm3 = request_data['tpluviometer3']
+    am = request_data['tanemometer']
+    vane_str = request_data['twd']
+    sm = request_data['tSoil_moist']
+    temp = request_data['ttemp']
+    humd = request_data['thumd']
+    pres = request_data['tpres']
+    lum = request_data['tLuminosity']
+    bat = request_data['tbat']
+    timex = request_data['ttime']
+
+    sensordata = appdir.models.SensorData(NodeID=NodeID, tpluviometer1=pm1, tpluviometer2=pm2, tpluviometer3=pm3, tanemometer=am, twd=vane_str, tSoil_moist=sm, ttemp=temp, thumd=humd, tpres=pres, tLuminosity=lum, tbat=bat, ttime=timex)
+    db.session.add(sensordata)
+    db.session.commit()
+    sensors = appdir.models.SensorData.query.all()
+    return render_template('data_.html', sensors=sensors)
 
 @app.route("/data_")
 def data_():
